47755a29df450c21ca9907c7d5e079be5e2a5091
LucianAlexander <lemnariulucianalexandru@yahoo.it>
2018-08-11 13:04:36 +0200

    Update delete.php

--- API/delete.php
+++ API/delete.php
@@ -2,22 +2,14 @@
     require "init.php";
     
     $tel = $_POST["tel"];
- 
-    
-    $statement = mysqli_prepare($con, "DELETE FROM persone WHERE peer_tel = ?");
-    mysqli_stmt_bind_param($statement, "s", $tel);
-    mysqli_stmt_execute($statement);
-    
-    mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement);
-    
-    $response = array();
-    $response["success"] = false;  
-    
-    while(mysqli_stmt_fetch($statement)){
-         $response["success"] = true;   
-    }
+    $stmt = $con->prepare("DELETE FROM persone WHERE peer_tel = ?");
+    $stmt->bind_param("s", $tel);
 
+   
+    $response["success"] = false; 
+    $result = $stmt->execute();
+    $response["success"]= $result;
+    
      if($response["success"] == true){
           $sql = "SELECT peer_nome, peer_cognome, peer_indirizzo, peer_tel, peer_eta FROM persone  ORDER BY persone.peer_id DESC";
           $result = mysqli_query($con,$sql);
